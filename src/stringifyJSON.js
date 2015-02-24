1c2b6d5622d8762a9713410b3e91ca27e9208807
Paul Brady <pablo.brady@gmail.com>
2015-02-24 15:26:19 -0800

    progress...

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,55 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
+// console.log(stringifyJSON([1, 'lazy', [ 4, 5, 6], false]));
 
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+console.log(obj);
+
+  var retStr = "";
+
+  // var object = function objectIsEmpty( o ) {
+  //   for ( var p in o ) { 
+  //     if ( o.hasOwnProperty( p ) ) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
+
+  // if (!objectIsEmpty(obj)) {
+  //   return obj;
+  // }
+
+  // if (!obj) {
+  //   return obj;
+  // }
+
+  var count = 0;
+  for ( var p in obj ) { 
+    var delimiter = "";
+
+    if(count>=1) { delimiter=","; }
+
+    if((typeof obj[p] === "number") || (typeof obj[p] === "boolean")) {
+      retStr += delimiter + obj[p];
+      // delete obj[p];
+    } else if(typeof obj[p] === "string") {
+      retStr += delimiter + "\"" + obj[p] + "\"";
+      // delete obj[p];
+    } else if(typeof obj[p] === "object") {
+      console.log("Recursion...");
+      retStr += delimiter + stringifyJSON( obj[p] );
+      // delete obj[p];
+    }
+    count++;
+  };
+  console.log("retStr = " + retStr);
+
+
+  return obj;
 };
+
+console.log( stringifyJSON( [1, [2,4,6], "three"] ) ) ;
+
