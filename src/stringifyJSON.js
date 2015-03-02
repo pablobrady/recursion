0f8460a641d2c30ff167ce3a49e2d4cce2049a1a
MKS-Student <mks.student@hackreactor.com>
2015-03-02 15:46:15 -0800

    progress

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,73 +1,65 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
-// console.log("Goal:  " + stringifyJSON([1, [2,4,6], "three"]));
 
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  var retStr = "";
-  var count = 0;
-  var delimiter = "[";
+  var array = [];
 
-  // console.log('' + obj + ' (' + typeof obj + '):');
+	if ( Array.isArray(obj) ) {
+		var buffer = [];
 
-  var stringifiableValue = function(testValue) {
-    if (testValue === undefined ) { return false; };
-    if (typeof testValue === "function") { return false; };
-    return true; 
-  }
+		for (var i = 0; i < obj.length; i++) {
+			if(typeof obj[i] === "string") {
+				buffer.push( '"' + obj[i] + '"' );
+			} else if ( Array.isArray(obj[i]) ) {
+				buffer.push( stringifyJSON(obj[i]) );
+			} else if ( typeof obj[i] === "object" ) {
+				buffer.push( stringifyJSON(obj[i]) );
+			} else {
+				buffer.push( obj[i] );
+			}
+		};
 
-  // EMPTY TESTS...
-  if( obj === undefined ) {
-    return;
+		array.push( '[' +  buffer.join() + ']' );
+	
 
-  } else if( obj === null ) {
-    return 'null';
+		return array.join();
+	}
+
+	if ( typeof obj === "object" ) {
+		var oBuffer = [];
+
+		if(obj===null) {
+			return 'null';
+		}
+
+		for (var key in obj) {
+			if ( typeof obj[key] === "function"  || typeof obj[key] === "undefined") {
+				continue;
+			} else if ( typeof obj[key] == "string") {
+				oBuffer.push( '"' + key + '":"' + obj[key] + '"' );
+			} else if(obj[key]===null) {
+				oBuffer.push( '"' + key + '":' + 'null' );
+		  } else if ( typeof obj[key] === "object" ) {
+		  	oBuffer.push( '"' + key + '":' + stringifyJSON(obj[key]) );
+		  } else {
+				oBuffer.push( '"' + key + '":' + obj[key] );
+			}
+		}
+
+		array.push ('{' + oBuffer + '}');
+
+		return array.join();
+	}
 
 
-  // ARRAY TESTS...
-  } else if( Array.isArray(obj) && obj.length==0 ){
-    return '[]';
+	if(typeof obj == "string"){
+		return '"' + obj + '"';
+	}
 
-  } else if( Array.isArray(obj) && obj.length>0 ){
-    retStr += '[';
-    for (var arrIdx = 0; arrIdx < obj.length; arrIdx++) {
-      retStr += (arrIdx>0 ? "," : "") + stringifyJSON( obj[arrIdx] );
-    };
-    retStr += ']';
-    return retStr;
-
-
-  // PRIMITIVE TESTS...
-  } else if( (typeof obj === 'number') ){
-    return String( obj ); 
-
-  } else if ( typeof obj === 'boolean' ){
-    return '' + obj + '';
-
-  } else if ( typeof obj === 'string' ) {
-    return '"' + obj + '"';
-
-    
-  
-  } else if ( typeof obj === 'object' ) {
-    retStr += "{";
-    var oCount = 0;
-    for ( var oKey in obj ) { 
-      if ( stringifiableValue(obj[oKey])==false ) {
-        retStr += '}';
-        return retStr;
-      } else {
-        retStr += (oCount>0 ? ',' : '') + '"' + oKey + '":' + stringifyJSON( obj[oKey] );
-        oCount++;
-     }
-    };
-    retStr += "}";
-    return retStr;
-
-  }
-
-  // Dropthru... shouldn't happen.
-  return retStr + ']';
+  return "" + obj;
 };
+
+
 
